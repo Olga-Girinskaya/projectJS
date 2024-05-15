0ffd2c2cda2c9c0fa27735ed4cbbafbdc7f28f20
Olga <girinskaya.olga@gmail.com>
2024-05-15 22:53:27 +0300

    commit 9

--- js/Exercises/exercise_14.js
+++ js/Exercises/exercise_14.js
@@ -1,18 +1,18 @@
 'use strict'
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi'];
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi','Tom','Olga','Igor'];
 
 function sortStudentsByGroups(arr) {
     let groupArr = [];
     let strArr = [];
     let str = '';
-    let index = Math.floor(arr.length/3)*3;
+    //let index = Math.floor(arr.length/3)*3; // так как групп только 3, то этот расчет не актуален
 
     for (let i = 0; i < arr.sort().length; i+=3){
-        if(i< index)
+        if(i < 9) // задаем сразу кол-во игроков в 3-х командах 3*3
         groupArr.push(arr.slice(i, i + 3)); }
 
-    for (let i = index; i < arr.sort().length; i++){
+    for (let i = 9; i < arr.sort().length; i++){
      strArr.push(arr[i]);         
     };
     str += `Оставшиеся студенты: ${strArr.length === 0 ? '-' : strArr.join(', ')}`;
